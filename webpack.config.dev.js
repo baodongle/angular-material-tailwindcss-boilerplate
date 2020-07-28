db3301117611b9783d3baa9be245acc7e08a6fbc
baodongle <baodongle@gmail.com>
2020-07-28 14:57:14 +0700

    initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              syntax: 'postcss-scss',
+              plugins: () => [require('postcss-import'), require('tailwindcss'), require('autoprefixer')],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+};
